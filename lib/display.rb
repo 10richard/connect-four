4f9844658e4b468808481859f123d7dc90d24ae5
Richard Ten <rten04@icloud.com>
2023-03-02 20:26:29 -0500

    Add/modify methods

--- lib/display.rb
+++ lib/display.rb
@@ -1,8 +1,16 @@
 module Display
 
-    def instructions(msg)
+    def print_board(board)
+        puts '1 2 3 4 5 6 7'
+        board.each do |row|
+            puts row.join(' ')
+        end
+    end
+
+    def instructions(msg, current_player=nil)
         {
-            'enter_names' => 'ENTER PLAYER NAMES'
+            'enter_names' => 'ENTER PLAYER NAMES',
+            'get_move' => "#{current_player}'s turn:\nEnter a column on the board (#1 - 7)",
         }[msg]
     end
 
