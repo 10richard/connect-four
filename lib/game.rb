f4c4c47ae286c6c67d83dbdea06212382204d726
Richard Ten <rten04@icloud.com>
2023-03-14 00:18:28 -0400

    Change player symbols to colored circles

--- lib/game.rb
+++ lib/game.rb
@@ -1,17 +1,21 @@
 require_relative './display.rb'
 require_relative './board.rb'
+require_relative './colors.rb'
 
-class Game < Board
+class Game
 
     include Display
+
+    attr_accessor :current
 
     def initialize
         #@player = {'name' => 'Patrick', 'symbol' => 'P'}
         @player1 = {}
         @player2 = {}
-        @current = nil
+        @current = @player1
         @game_over = nil
         @board = Board.new()
+        @p_board = @board.board
     end
 
     def get_names
@@ -33,46 +37,50 @@ class Game < Board
         end
         @player1['name'] = p1
         @player2['name'] = p2
+        set_player_symbols
         puts 'Player names set'
     end
 
     def set_player_symbols
-        @player1['symbol'] = @player1['name'][0]
-        @player2['symbol'] = @player2['name'][0]
+        @player1['symbol'] = '◉'.red
+        @player2['symbol'] = '◉'.blue
     end
 
     def play
         get_names
         @current 
         until @game_over
-            print_board(@board)
+            @p_board = @board.board
+            print_board(@p_board)
             move = get_move
-            modify_board(move, @current)
+            @board.modify_board(move, @current)
             @gameover = game_over?
             @current == @player1 ? @current = @player2 : @current = @player1
         end
         check_outcome
+        print_board(@board.board)
     end
 
     def get_move
         #append move to current player's moves list? - use to determine if there is a winner
         got_move = false
         until got_move
-            puts instructions('get_move')
-            move = gets.chomp
+            puts instructions('get_move', @current['name'])
+            move = gets.chomp.to_i
             got_move = validate_move(move)
         end
-        @current == @player1 ? @player1['moves'].append(move) : @player2['moves'].append(move)
+        move
     end
 
     def validate_move(move)
         return true if move.between?(1, 7) && !column_full?(move)
+        puts move = '' ? game_error('no_column') : game_error('column_full')
         #check if column is full
         #if column full, then print error and return false
     end
 
     def column_full?(move)
-        column = @board.map { |row| row[move - 1]}
+        column = @p_board.map { |row| row[move - 1]}
 
         #checks if column has no empty spaces ("O")
         column.none?('O')
@@ -85,7 +93,7 @@ class Game < Board
         #if there is a winner change the variable to player1 or player2 
 
         #check_winner method is located in board.rb
-        if check_winner
+        if @board.check_winner(@current)
             @game_over = @current
         elsif draw?
             @game_over = 'draw'
@@ -93,7 +101,7 @@ class Game < Board
     end
 
     def draw?
-        @board.each do |row|
+        @p_board.each do |row|
             return false if row.any?('O')
         end
         true
